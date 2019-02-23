c6713c59176e10a269bd8ebf3db9e4ce2e9a3433
Luke Winikates <luke.winikates@gmail.com>
2019-02-22 17:56:34 -0800

    clean up spotify playback event listeners; move placeholder text from HTML into JS

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
+let setupSpotifyDebugging = (player) => {
+  let spotifyPlayerEvents = ['initialization_error',
+    'authentication_error',
+    'account_error',
+    'playback_error',
+    'ready',
+    'not_ready'];
+
+  spotifyPlayerEvents.forEach(event => {
+      player.addListener(event, (eventMessage) => {
+        console.log(event, eventMessage)
+      });
+    }
+  );
+};
 
 window.init = ({songTitleElement, window}) => {
-  let state = {};
+  let state = {
+    songTitle: '(Waiting for Spotify Player to Connect)'
+  };
 
-  let redrawUI = () => {
+  let draw = () => {
     songTitleElement.innerText = state.songTitle || "No Song Playing";
   };
 
@@ -10,36 +27,21 @@ window.init = ({songTitleElement, window}) => {
   window.onSpotifyWebPlaybackSDKReady = () => {
     const token = window.spotifyKey;
     window.player = new Spotify.Player({
-      name: 'Web Playback SDK Quick Start Player',
+      name: 'Shibusawa Lyric Timer',
       getOAuthToken: cb => {
         cb(token);
       }
     });
-    // Error handling
-    player.addListener('initialization_error', ({ message }) => { console.error("init failed"); });
-    player.addListener('authentication_error', ({ message }) => { console.error("auth failed"); });
-    player.addListener('account_error', ({ message }) => { console.error(message); });
-    player.addListener('playback_error', ({ message }) => { console.error(message); });
 
-    // Playback status updates
     player.addListener('player_state_changed', playerState => { console.log(playerState);
       state.songTitle = playerState.track_window.current_track.name;
-      redrawUI();
+      // TODO: throws an error if playerState is null; playerState is null when player disconnects
+      draw();
     });
 
-    // Ready
-    player.addListener('ready', ({ device_id }) => {
-      console.log('Ready with Device ID', device_id);
-    });
-
-    // Not Ready
-    player.addListener('not_ready', ({ device_id }) => {
-      console.log('Device ID has gone offline', device_id);
-    });
-
-    // Connect to the player!
+    setupSpotifyDebugging(player);
     player.connect();
-
-
   };
+
+  draw();
 };
